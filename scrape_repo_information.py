c0495c4e0f1325a1ba0af9c7590e233b66a7f3de
Avyay Varadarajan <avyayv@gmail.com>
2018-10-28 22:01:12 -0700

    fixed some formatting; easier human reading

--- scrape_repo_information.py
+++ scrape_repo_information.py
@@ -1,4 +1,3 @@
-from bs4 import BeautifulSoup
 import urllib.request
 import pdb
 import xml.etree.ElementTree as xml
@@ -15,73 +14,38 @@ Stem counter by sushain
 
 pairs = []
 Pair = collections.namedtuple('Pair', 'lg1 lg2 last_updated created direction repo stems')
+types = ["trunk", "nursery", "incubator", "staging"]
 
-types = ["trunk", "incubator", "nursery", "staging"]
+def print_info(uri):
+    returned = get_info(uri)
+    return returned['stems']
 
-def print_info(uri, bidix=None):
-    returned = get_info(uri, bidix)
-    if "stems" in returned:
-        return returned['stems']
-    if "paradigms" in returned:
-        print('Paradigms: %s' % returned['paradigms'])
-
-def get_info(uri, bidix=None):
-    dictX = ""
-    if "http" in uri:
-        try:
-            dictX = str((urllib.request.urlopen(uri)).read(), 'utf-8')
-        except:
-            return -1 # FIXME: error handling
-
-    else:
-        dictX = (open(uri, 'r')).read()
+def get_info(uri):
     try:
+        dictX = str((urllib.request.urlopen(uri)).read(), 'utf-8')
         tree = xml.fromstring(dictX)
     except:
-        return -1  # FIXME: error handling
+        return -1 # FIXME: error handling
 
-    if bidix is not None:
-        bi = bidix
-    else:
-        bi = len(tree.findall("pardefs")) == 0 #bilingual dicts don't have pardefs section -- not necessarily true? check /trunk/apertium-en-es/apertium-en-es.en-es.dix
-
-    bi = True
     out = {}
-    if(bi):
-        out['stems'] = len(tree.findall("*[@id='main']/e//l"))
-    else:
-        out['stems'] = len(tree.findall("section/*[@lm]"))  # there can be sections other than id="main"
-        if tree.find('pardefs') is not None:
-            out['paradigms'] = len(tree.find('pardefs').findall("pardef"))
+    out['stems'] = len(tree.findall("*[@id='main']/e//l"))
     return out
 
-
-
-
 for x in types:
-    html_data = urllib.request.urlopen('https://api.github.com/repos/apertium/apertium-'+x+'/'+'contents')
+    html_url = "https://api.github.com/repos/apertium/apertium-%s/contents" % x
+    html_data = urllib.request.urlopen(html_url)
     html = json.loads(html_data.read())
     for i in html:
-        try:
-            if "apertium" in i["name"]:
-                lg1 = ""
-                lg2 = ""
-                direction = ""
-                created = ""
-                last_updated = ""
+        if "apertium" in i["name"]:
+            try:
                 repo_name = x
-                stems = 0
-
+                direction = ""
                 lang_pair_name = i["name"]
-                #link = i["download_url"]
-
                 #getting names
-                important = lang_pair_name.split('-')
-                lg1 = important[1]
-                lg2 = important[2]
-
+                lg1 = lang_pair_name.split('-')[1]
+                lg2 = lang_pair_name.split('-')[2]
                 #getting into repository
-                link = "https://api.github.com/repos/apertium/"+lang_pair_name+"/contents"
+                link = "https://api.github.com/repos/apertium/%s/contents" % lang_pair_name
                 repo_json = json.loads(urllib.request.urlopen(link).read())
                 for el in repo_json:
                     if el["name"] == "modes.xml":
@@ -91,25 +55,22 @@ for x in types:
                             direction += "<"
                         if lg2+'-'+lg1 in str(html_for_blob):
                             direction += ">"
-
                     elif ".dix" in el["name"]:
-                        stems = print_info(el["download_url"], bidix=True)
-
+                        stems = print_info(el["download_url"])
                     if direction == "><":
                         direction = "<>"
-
-                commits_link = 'https://api.github.com/'+'repos/apertium/'+lang_pair_name.strip()+"/commits"
+                commits_link = "https://api.github.com/repos/apertium/%s/commits" % lang_pair_name
                 commits_html = urllib.request.urlopen(commits_link).read()
-
                 last_updated = json.loads(commits_html)[0]["commit"]["committer"]["date"]
                 created = json.loads(commits_html)[-1]["commit"]["committer"]["date"]
-
                 pair = Pair(created=created, last_updated=last_updated, lg1=lg1.strip(), lg2=lg2.strip(), direction=direction, repo=repo_name, stems=stems)
-                print(json.dumps(pair, default=lambda o: o.__dict__))
+                print(json.dumps(pair, default=lambda o: dict(o)))
                 pairs.append(pair)
                 time.sleep(3) #if you change this value you will get blocked likely
-        except:
-            pass
+                
+            except (IndexError, TypeError):
+                pass #this happens if there is a weird entry in the repo
 
-with open('pairs.json', 'a') as f:
-    json_string = json.dumps([ob.__dict__ for ob in pairs])
+if __name__ == "__main__":
+    with open('pairs.json', 'a') as f:
+        json_string = json.dumps([dict(ob) for ob in pairs])
