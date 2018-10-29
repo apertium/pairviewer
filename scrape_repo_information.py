0a8e489a84222f6efb61a41ca77f545fd16e5d1c
Avyay Varadarajan <avyayv@gmail.com>
2018-10-29 15:56:45 -0700

    fixed issues with stem and dix

--- scrape_repo_information.py
+++ scrape_repo_information.py
@@ -18,6 +18,7 @@ CLIENT_SECRET = os.environ['GITHUB_SECRET_CLIENT_ID']
 pairs = []
 Pair = collections.namedtuple('Pair', 'lg1 lg2 last_updated created direction repo stems')
 types = ["trunk", "nursery", "incubator", "staging"]
+
 params = urllib.parse.urlencode(dict({'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET}))
 
 def print_info(uri):
@@ -33,13 +34,15 @@ def get_info(uri):
 
     return len(tree.findall("*[@id='main']/e//l"))
 
-for x in types:
-    html_url = "https://api.github.com/repos/apertium/apertium-%s/contents?" % x
-    html_data = urllib.request.urlopen(html_url + params)
-    html = json.loads(html_data.read())
-    for i in html:
-        if "apertium" in i["name"]:
-            try:
+
+
+if __name__ == "__main__":
+    for x in types:
+        types_html_url = "https://api.github.com/repos/apertium/apertium-%s/contents?" % x
+        types_html_data = urllib.request.urlopen(types_html_url + params)
+        types_html = json.loads(types_html_data.read())
+        for i in types_html:
+            if "apertium" in i["name"] and len(i["name"].split('-')) == 3:
                 repo_name = x
                 direction = ""
                 lang_pair_name = i["name"]
@@ -52,27 +55,28 @@ for x in types:
                 for el in repo_json:
                     if el["name"] == "modes.xml":
                         download_url = el["download_url"]
-                        html_for_blob = urllib.request.urlopen(download_url).read()
-                        if lg1+'-'+lg2 in str(html_for_blob):
-                            direction += "<"
-                        if lg2+'-'+lg1 in str(html_for_blob):
-                            direction += ">"
-                    elif ".dix" in el["name"]:
+                        html_for_blob_utf_8 = str((urllib.request.urlopen(download_url)).read(), 'utf-8')
+                        tree = xml.fromstring(html_for_blob_utf_8)
+                        for child in tree:
+                            if lg1+'-'+lg2 == child.attrib["name"] and "<" not in direction:
+                                direction += "<"
+                            if lg2+'-'+lg1 in child.attrib["name"] and ">" not in direction:
+                                direction += ">"
+
+                    elif el["name"] == "apertium-%s.%s.dix" % ((lg1+'-'+lg2), (lg1+'-'+lg2)):
                         stems = print_info(el["download_url"])
+
                     if direction == "><":
                         direction = "<>"
+
                 commits_link = "https://api.github.com/repos/apertium/%s/commits?" % lang_pair_name
                 commits_html = urllib.request.urlopen(commits_link + params).read()
                 last_updated = json.loads(commits_html)[0]["commit"]["committer"]["date"]
                 created = json.loads(commits_html)[-1]["commit"]["committer"]["date"]
                 pair = Pair(created=created, last_updated=last_updated, lg1=lg1.strip(), lg2=lg2.strip(), direction=direction, repo=repo_name, stems=stems)
-                print(json.dumps(pair, default=lambda o: dict(o)))
+                print(json.dumps(pair._asdict(), default=lambda o: dict(o)))
                 pairs.append(pair)
-                time.sleep(0.1) #if you change this value you will get blocked likely
 
-            except (IndexError, TypeError):
-                pass #this happens if there is a weird entry in the repo
-
-if __name__ == "__main__":
     with open('pairs.json', 'a') as f:
-        json_string = json.dumps([dict(ob) for ob in pairs])
+        json_string = json.dumps([ob._asdict() for ob in pairs])
+        f.write(json_string)
